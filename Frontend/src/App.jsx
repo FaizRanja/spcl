e499bce2d2a93bee5662e4ee8edaf6e3ae818a7e
hamdan0006 <hamdanahmad0006@gmail.com>
2025-11-27 13:23:04 +0500

    league section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,12 @@ import Bookmakers from './pages/Bookmakers';
 import Leagues from './pages/Leagues';
 import MathPredictions from './pages/MathPredictions';
 import LiveScore from './pages/LiveScore';
+import Football from './pages/Football';
+import Basketball from './pages/basketball';
+import Tennis from './pages/Tennis';
+import BestBettingapp from './pages/bestBettingapps';
+import PopularLeagues from './pages/PopularLeagues';
+
 
 function App() {
   return (
@@ -20,6 +26,16 @@ function App() {
         <Route path="/football/competitions/" element={<Leagues />} />
         <Route path="/math-predictions" element={<MathPredictions />} />
         <Route path="/livescore" element={<LiveScore />} />
+        <Route path="/predictions/football" element={<Football />} />
+        <Route path="/predictions/basketball" element={<Basketball />} />
+        <Route path="/predictions/tennis" element={<Tennis />} />
+        <Route path="/ar/bookmakers" element={<BestBettingapp />} />
+
+
+
+          <Route path="/league/:leagueId" element={<PopularLeagues />} />
+
+
       </Routes>
 
       <Footer />
